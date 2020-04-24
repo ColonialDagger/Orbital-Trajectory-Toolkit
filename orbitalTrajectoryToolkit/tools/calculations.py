f6879c08fe9ef92f8c997a040b418cb57d5fd6e1
Daniel Garcia <dannyg305@hotmail.com>
2020-04-23 20:13:44 -0400

    Increased error checking for deltav() and corrected twr() formula

--- orbitalTrajectoryToolkit/tools/calculations.py
+++ orbitalTrajectoryToolkit/tools/calculations.py
@@ -1,11 +1,15 @@
 import numpy
 
 
-def deltav(wet, dry, exv, mode):
-    if mode == 0:
-        exv = exv*9.80665
-    return exv*numpy.log((wet/dry))
+def deltav(wet, dry, exv, mode):  # Mode 0 for ISP input, mode 1 for ExVel input
+    if dry <= wet:
+        if mode == 0:
+            exv = exv * 9.80665
+            # TODO: DOES G FOR ISP CHANGE BASED ON ORBITAL BODY?
+        return exv*numpy.log((wet/dry))
+    else:
+        raise ValueError
 
 
-def twr(f, w):
-    return f/w
+def twr(f, w, g):
+    return f/(w*g)
