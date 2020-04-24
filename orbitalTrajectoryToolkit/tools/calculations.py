6ae61d58a5da980dd9fe8a8600f59e3f20997198
Daniel Garcia <dannyg305@hotmail.com>
2020-04-24 12:34:55 -0400

    Implemented isp/exvel conversion

--- orbitalTrajectoryToolkit/tools/calculations.py
+++ orbitalTrajectoryToolkit/tools/calculations.py
@@ -1,15 +1,25 @@
 import numpy
+import math
 
 
 def deltav(wet, dry, exv, mode):  # Mode 0 for ISP input, mode 1 for ExVel input
     if dry <= wet:
         if mode == 0:
-            exv = exv * 9.80665
+            exv = isp_exvel(exv)
             # TODO: DOES G FOR ISP CHANGE BASED ON ORBITAL BODY?
         return exv*numpy.log((wet/dry))
     else:
         raise ValueError
 
 
+def isp_exvel(exv):  # ISP to Exhaust Velocity conversion
+    return exv * 9.80665
+
+
 def twr(f, w, g):
     return f/(w*g)
+
+
+def burntime(wet, exv, f, dv):  # TODO: Figure out burn time calculation
+    time = (1-math.pow(math.e, (numpy.negative(dv)/exv)))*((wet*exv)/f)
+    return time
